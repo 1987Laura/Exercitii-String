3a1e88113656ce3d151939456befbb9bc2e6e6dd
Laura Popescu <laura.liliana.popescu@gmail.com>
2025-02-19 12:44:09 +0200

    Primele 10 exercitii

---
+++ 100_exercitii_string.py
@@ -0,0 +1,47 @@
+#ex1
+
+text1 = "Hello"
+text2 = "World"
+print(text1+text2)
+    
+#ex2
+text = " all right "
+print(text * 3)
+
+#ex3
+text = "10" + "0" * 3
+print(text)
+
+#ex4
+text = "3.1415"
+number = 3.1415
+print(text == number)
+
+#ex5
+text1 = "not all strings are create equal"
+text2 = '''not all strings are create equal'''
+print(text1 == text2)
+
+#ex6
+
+text1 = """strings are awesome"""
+text2 = "strings are awesorne"
+print(text1 != text2)
+
+#ex7
+text1 = """awesome"""
+text2 = "awesome"
+print(text1 is text2)
+
+#ex8 
+text = "hello"
+print("L" in text)
+
+#ex9
+text = " Python is great."
+print("python" not in text)
+
+#ex10
+adjective = "Awesome"
+print(not adjective)
+      
